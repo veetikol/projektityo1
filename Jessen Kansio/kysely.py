f5f8a2d16adf440dbacb46cfc68589fb78761de1
Jesse Nieminen <j.c.nieminen@gmail.com>
2023-09-26 15:14:55 +0300

    Changes by LENOVO

---
+++ Jessen Kansio/kysely.py
@@ -0,0 +1,60 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+luxembourghints = ["1. The Second richest country in the world", "2. The name of this country starts with a word from latin, which means “light”", "3. The Smallest country in Benelux"]
+norwayhints = ["1. The most successful country in Winter Olympics", "2. This country has the highest standard of living in the world", "3. This country shares border with 3 other countries but it has more coastline than land border."]
+polandhints = ["1. The biggest country in eastern middle-europe", "2. The flag of this country is like Pokemon ball", "3. During WWII. Germany built concentration camps in this country"]
+swedenhints = ["1. Dancing queen", "2. The world leading furniture company was founded in this country", "3. This country is always one step ahead of Finland"]
+latviahints = ["1. The jeans were invented in this country", "2. This country is sandwiched by other baltic countries", "3. The capital of this country is Riga"]
+lithuaniahints = ["1. This country is known from its amber jewellery", "2. The biggest baltic country", "3. The capital of this country is Vilnius"]
+spainhints = ["1. One of the biggest symbol of this country is a bull", "2. You can go to Africa with a ferry from this country in 1 hour", "3. The Finnish tourists have invaded this country"]
+albaniahints = ["1. The flag of this country has 2 headed eagle on it", "2. This European country is one of the last ones that doesn’t have McDonalds", "3. 70% of this country is mountain area and its located opposite side of the “high heel”"]
+bulgariahints = ["1. The Europes poorest country", "2. This country uses cyrillic alphabet", "3. The capital of this country is Sofia"]
+icelandhints = ["1. This country is a Nordic country", "2. This country is home to the artist Björk", "3. This country has hot springs"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+croatiahints = ["1. Best football player of the year 2018 chosen by FIFA born here", "2. The famous dessert with bananas references to one of the biggest cities in this country", "3. One of the filming places for Game Of Thrones"]
+francehints = ["1. A significant country in the world of fashion art", "2. The Disney animation about a cooking mouse is based on this country", "3. Known for its over 300 meters long tower"]
+greecehints = ["1. This country has the third oldest language in the world", "2. A country known for the development of philosophy", "3. This country is the birthplace of the modern Olympics"]
+italyhints = ["1. Leads UNESCO's statistics on world heritage sites", "2. Milla Magia lives on the slopes of a famous volcano in this country", "3. This country is known worldwide for its delicious pizza and pasta"]
+sloveniahints = ["1. World famous 'Postojna' cave system is located in this country", "2. This country is mainly mountainous, and more than 90 percent of the country is more than 200 meters above sea level", "3. The capital of this country is a charming city named Ljubljana"]
+czechrepublichints = ["1. World known ice-hockey player with a iconic mullet haircut was born here", "2. Federation which splitted into two separated countries in 1993", "3. Pilsner beer originates from this country"]
+maltahints = ["1. Is known by taxfree casino licenses", "2. This country consista by many islands", "3. World's tenth-smallest country. With a total area of 316 squeare kilometers"]
+hungaryhints 
--- game/game.py
+++ game/game.py
@@ -1,2 +1,36 @@
+from geopy.distance import geodesic
 import mysql.connector
+import sqlyhteys
+from kuljettumatka import calculateDistance
+
+rahat = 0 # Rahan määrää pitänee kontrolloida jokaisen funktion sisällä.
+pelaajanimi is None # input
+sijainti is None # Tämä määritellään sql-haulla
+vihjeet # Tämä on siis lista vihjeitä
+tavoitemaa is None # Eli tämä haetaan sql-haulla.
+
+pelaajanimi = input(f"Tervetuloa pelaamaan! Mikä on nimesi?: ")
+print(f"Hei {pelaajanimi}!")
+def instructions():
+    print("Olet matkustamassa Europassa. Vanhempasi ovat suunnitelleet sinulle reitin eri maiden läpi, "
+        "ja antaneet sinulle 1000 euroa kustannuksiin.")
+    print("Saavuttuasi lentokentälle tehtäväsi on arvata seuraavan kohteen nimi. Saat tehtävän alussa yhden vihjeen,"
+      "ja voit halutessasi ostaa lisää vihjeitä sinulle annetuilla rahoilla. 3 yrityksen jälkeen sinulta veloitetaan"
+      "sakko (5 euroa)")
+
+readyInput = input("Oletko valmis aloittamaan? (k/e): ")
+if readyInput == "e":
+    instructions()
+else:
+    # tähän funktio, joka jatkaa peliä
+
+
+
+
+'''Tähän tulee siis funktio, joka arpoo aloitusmaan'''
+'''Tähän tulee funktio, joka arpoo kohdemaan'''
+'''Pelaaja etenee vaiheeseen, jossa hän voi ostaa uuden vihjeen, tai lentää kohteeseen'''
+'''Eli syötteen perusteella joko funktio, joka antaa uuden vihjeen, tai sitten funktio, joka siirtää pelaajan valittuun maahan'''
+'''Siirtymisen jälkeen funktio, joka tsekkaa, onko pelaaja oikeassa maassa.'''
+
 
