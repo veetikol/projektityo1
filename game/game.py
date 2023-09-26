cf509d0aa4877db8d2317e160a71bfb8ab253b38
Jesse Nieminen <j.c.nieminen@gmail.com>
2023-09-26 16:21:59 +0300

    Changes by LENOVO

--- game/game.py
+++ game/game.py
@@ -2,27 +2,40 @@ from geopy.distance import geodesic
 import mysql.connector
 import sqlyhteys
 from kuljettumatka import calculateDistance
+from SqlKoodiMaaRand import maat
+from Pelaajavalinnat import maatsekki, pelaajavalinta, maatsekki
+import Vihjeet
+
 
 rahat = 0 # Rahan määrää pitänee kontrolloida jokaisen funktion sisällä.
-pelaajanimi is None # input
-sijainti is None # Tämä määritellään sql-haulla
+tavoite = maat() # Tämä määritellään sql-haulla
 vihjeet # Tämä on siis lista vihjeitä
 tavoitemaa is None # Eli tämä haetaan sql-haulla.
 
-pelaajanimi = input(f"Tervetuloa pelaamaan! Mikä on nimesi?: ")
-print(f"Hei {pelaajanimi}!")
-def instructions():
-    print("Olet matkustamassa Europassa. Vanhempasi ovat suunnitelleet sinulle reitin eri maiden läpi, "
-        "ja antaneet sinulle 1000 euroa kustannuksiin.")
-    print("Saavuttuasi lentokentälle tehtäväsi on arvata seuraavan kohteen nimi. Saat tehtävän alussa yhden vihjeen,"
-      "ja voit halutessasi ostaa lisää vihjeitä sinulle annetuilla rahoilla. 3 yrityksen jälkeen sinulta veloitetaan"
-      "sakko (5 euroa)")
+pelaajanimi = input(f"Welcome! What's your name?: ")
+print(f"Hello {pelaajanimi}!")
 
-readyInput = input("Oletko valmis aloittamaan? (k/e): ")
-if readyInput == "e":
-    instructions()
-else:
-    # tähän funktio, joka jatkaa peliä
+def instructions():
+    print("You are travelling in Europe. Your parents have planned a route for you through various countries, "
+        "and given you 1000 euros for expenses.")
+    print("When you arrive at an airport you are tasked with guessing the name of the next country in your route. ")
+    print("You are given one hint at first, and you can buy more hints with the money you've been given.")
+    print("After 3 tries, you will be given a sanction of 5 euros and flown to the next country.")
+
+while True:
+    readyInput = input("Are you ready to begin? (y/n): ")
+    if readyInput == "n":
+        print("Here are the instructions again: ")
+        instructions()
+    else:
+        break
+
+print("Your journey begins!")
+print(f"The first country you land in is: {sijainti}! You have a lot of fun touring the different attractions there.")
+
+while True:
+    print(f"")
+
 
 
 
