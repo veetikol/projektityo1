e03d9810c9b11f657b2083aa0ed1d25027dc0823
Jesse Nieminen <j.c.nieminen@gmail.com>
2023-09-26 16:21:43 +0300

    Changes by LENOVO

--- game/Vihjeet.py
+++ game/Vihjeet.py
@@ -1,26 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 luxembourghints = ["1. The Second richest country in the world", "2. The name of this country starts with a word from latin, which means “light”", "3. The Smallest country in Benelux"]
 norwayhints = ["1. The most successful country in Winter Olympics", "2. This country has the highest standard of living in the world", "3. This country shares border with 3 other countries but it has more coastline than land border."]
 polandhints = ["1. The biggest country in eastern middle-europe", "2. The flag of this country is like Pokemon ball", "3. During WWII. Germany built concentration camps in this country"]
@@ -31,25 +8,12 @@ spainhints = ["1. One of the biggest symbol of this country is a bull", "2. You 
 albaniahints = ["1. The flag of this country has 2 headed eagle on it", "2. This European country is one of the last ones that doesn’t have McDonalds", "3. 70% of this country is mountain area and its located opposite side of the “high heel”"]
 bulgariahints = ["1. The Europes poorest country", "2. This country uses cyrillic alphabet", "3. The capital of this country is Sofia"]
 icelandhints = ["1. This country is a Nordic country", "2. This country is home to the artist Björk", "3. This country has hot springs"]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+belgiumhints = ["1. This country's flag has the same colors as the flag of Germany", "2. This country, instead of France, invented french fries", "3. This country is often associated and mixed with France"]
+germanyhints = ["1. This country's history in the World War II is ver controversial", "2. This country is a major hub of the automotive industry", "3. This country is right in the middle of Europe"]
+estoniahints = ["1. This country was last one to become independent from Soviet Union", "2. This country is a neighbour country of Finland", "3. This country's capital is Tallinn"]
+finlandhints = ["1. This country is known for one of the most successful mobile phone businesses in the world", "2. This country is constantly ranked as one of the world's happiest countries", "3. This country has a very strong sauna culture"]
+ukhints = ["1. This country is known for bands like Blur", "2. This country is not a part of the EU", "3. This country is home to fish and chips"]
+irelandhints = ["1. This country is known for it’s pub culture", "2. This country is home to leprechauns", "3. This country’s capital is Dublin"]
 croatiahints = ["1. Best football player of the year 2018 chosen by FIFA born here", "2. The famous dessert with bananas references to one of the biggest cities in this country", "3. One of the filming places for Game Of Thrones"]
 francehints = ["1. A significant country in the world of fashion art", "2. The Disney animation about a cooking mouse is based on this country", "3. Known for its over 300 meters long tower"]
 greecehints = ["1. This country has the third oldest language in the world", "2. A country known for the development of philosophy", "3. This country is the birthplace of the modern Olympics"]
@@ -57,4 +21,19 @@ italyhints = ["1. Leads UNESCO's statistics on world heritage sites", "2. Milla 
 sloveniahints = ["1. World famous 'Postojna' cave system is located in this country", "2. This country is mainly mountainous, and more than 90 percent of the country is more than 200 meters above sea level", "3. The capital of this country is a charming city named Ljubljana"]
 czechrepublichints = ["1. World known ice-hockey player with a iconic mullet haircut was born here", "2. Federation which splitted into two separated countries in 1993", "3. Pilsner beer originates from this country"]
 maltahints = ["1. Is known by taxfree casino licenses", "2. This country consista by many islands", "3. World's tenth-smallest country. With a total area of 316 squeare kilometers"]
-hungaryhints = ["This country is known by its traditional "]
+hungaryhints = ["1. This country is known by its traditional spicy meat soup", "2. Local currency is known as HUF", "3. Tonava river splits this country at the middle"]
+austriahints = ["1. Samuli Edelmann has drunk wine in this country", "2. Most of the landscape of this country consists of the Alps", "3. In which country was Mozart born in?"]
+
+countries = {"Luxembourg":luxembourghints, "Norway":norwayhints, "Poland":polandhints, "Sweden":swedenhints, "Latvia":latviahints,
+             "Lithuania":lithuaniahints, "Spain":spainhints, "Albania":albaniahints, "Bulgaria":bulgariahints, "Iceland":icelandhints,
+             "Belgium":belgiumhints, "Germany":germanyhints, "Estonia":estoniahints, "Finland":finlandhints, "United Kingdom" or "UK":ukhints,
+             "Ireland":irelandhints, "Croatia":croatiahints, "France":francehints, "Greece":greecehints, "Italy":italyhints,
+             "Slovenia":sloveniahints, "Czech Republic":czechrepublichints, "Malta":maltahints, "Hungary":hungaryhints, "Austria":austriahints}
+
+for x in maat():
+    print(x)
+    command = input("Anna komento (Fly/Buy new hint)")
+    if command == "fly":
+        break
+    elif command == "Buy new hint":
+        pass
