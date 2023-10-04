6dcc0aaf4232e3b162a8344f2f60fece1e301a04
veetikol <vedek03@gmail.com>
2023-10-04 12:19:22 +0300

    Changes by vipe

---
+++ game/maaPicker.py
@@ -0,0 +1,60 @@
+import random
+import mysql.connector
+import sqlyhteys
+
+yhteys = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='flight_game',
+    user='root',
+    password='veetik',
+    autocommit=True
+    )
+
+
+
+
+import random
+import mysql.connector
+yhteys = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='flight_game',
+    user='user1',
+    password='password1',
+    autocommit=True
+)
+
+
+
+def maat():
+    sql = "SELECT country.name, airport.name FROM country, airport"
+    sql += " WHERE airport.iso_country = country.iso_country AND country.continent = 'EU' AND airport.type = 'large_airport'"
+    sql += " ORDER by RAND()"
+    kursori = yhteys.cursor(buffered=True)
+    kursori.execute(sql)
+    tulos = kursori.fetchone()
+    return tulos[0]
+
+
+
+def maaLentoSanakirja():
+    sql = "SELECT country.name, airport.name FROM country, airport"
+    sql += " WHERE airport.iso_country = country.iso_country AND country.continent = 'EU' AND airport.type = 'large_airport'"
+    sql += " ORDER by RAND()"
+    kursori = yhteys.cursor(buffered=True)
+    kursori.execute(sql)
+    countries = {}
+    while len(countries) < 37:  # tämä järjestää kaikki maat sanakirjaan
+        tulos = kursori.fetchone()
+        if tulos[0] != countries:
+            countries[tulos[0]] = tulos[1]
+    return countries
+
+poistaMaa = maaLentoSanakirja()
+tulos = maat()
+if tulos in poistaMaa:
+    print(tulos)
+    poistaMaa.pop(tulos)
+
+print(poistaMaa)
